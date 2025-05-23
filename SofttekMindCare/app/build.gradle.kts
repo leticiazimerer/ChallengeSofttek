b810970bbb0538b925312453cf378c15adfd2ef6
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-05-22 22:08:08 -0300

    feat(): Altera arquivos gradle

--- SofttekMindCare/app/build.gradle.kts
+++ SofttekMindCare/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
+    id("com.android.application")  // SEM version aqui!
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")  // ID corrigido
 }
 
 android {
@@ -43,14 +43,21 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
-    // Room Database
+    // Room
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Security
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
--- SofttekMindCare/build.gradle.kts
+++ SofttekMindCare/build.gradle.kts
@@ -1,65 +1,10 @@
 plugins {
-    id("com.android.application")
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
 }
 
-android {
-    namespace = "com.softtek.mindcare"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.softtek.mindcare"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-
-    // Room Database
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    implementation "com.google.code.gson:gson:2.10.1"
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Para PreferencesManager seguro
-    implementation "androidx.security:security-crypto:1.1.0-alpha06"
-
-    // Para WorkManager
-    implementation "androidx.work:work-runtime-ktx:2.9.0"
-
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
